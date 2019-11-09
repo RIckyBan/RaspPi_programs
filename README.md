4b7da54d20a804c4896a502c4418f78dfd4ce7a2
Riki Bando <ricky1231@g.ecc.u-tokyo.ac.jp>
2019-11-09 15:53:31 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# RaspPi_programs
+# My Face Detection
+
+![](eyecatch.png)
+
+Repo for ids practical training (term 2).
+
+[Slides](https://docs.google.com/presentation/d/14dNWCIf-SAUe0A68x3ppPXXHQDHBnrDXR3qVUE6XTyE/edit?usp=sharing)
+
--- post.sh
+++ post.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 URL="https://maker.ifttt.com/trigger"
-EVENTNAME="Raspberry_Pi" 
-YOUR_KEY="f-4s8Lcz2BkElbrZR7Tr9eJwEzNMauMXRwa283w31-E" 
+EVENTNAME="Raspberry_Pi"
+
+YOUR_KEY="" # Insert your key
 
 WEBHOOKSURL="${URL}/${EVENTNAME}/with/key/${YOUR_KEY}"
 
-curl -X POST -H "Content-Type: application/json" -d '{"value1":"'$1'","value2":"'$2'", "value3":"'$3'"}' ${WEBHOOKSURL} 
+curl -X POST -H "Content-Type: application/json" -d '{"value1":"'$1'","value2":"'$2'", "value3":"'$3'"}' ${WEBHOOKSURL}
 
 echo
 exit 0
